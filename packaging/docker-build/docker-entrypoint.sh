189b13c23624e246f42062e534a31c507f8bc422
Tibor Répási <rtib@users.noreply.github.com>
2025-01-15 08:15:27 +0100

    add assemblies to packages

--- packaging/docker-build/docker-entrypoint.sh
+++ packaging/docker-build/docker-entrypoint.sh
@@ -7,5 +7,7 @@ $@
 cd ${WORKDIR}/cql-metrics-collector
 dpkg-buildpackage -us -uc -b
 mv ../*.deb ${WORKDIR}/packages
+mv target/*.tar.gz ${WORKDIR}/packages
+mv target/*.zip ${WORKDIR}/packages
 dh clean
 cd ${WORKDIR}
